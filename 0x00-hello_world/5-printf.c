18db7ac5c65bc0a6bcccf5d95ddee9c30bdd75bd
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-16 21:18:42 -0800

    Prints a text using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
-# !/bin/bash
 #include <stdio.h>
 
  /**
  * main - Entry point
+ * 
+ * Description: The function print
+ * a text
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
