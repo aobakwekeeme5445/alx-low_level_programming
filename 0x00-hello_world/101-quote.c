145f26fcbc47c94f79ea4c6672108b556e9a68d3
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-16 22:29:54 -0800

    Print a quote to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio>
+#include <stderr>
+ /**
+  * main - Entry point
+  *
+  * Description: A program print a text
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	return (1);
+}
