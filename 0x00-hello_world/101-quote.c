edc62663fdc879cb8b6304a45ce642601149a951
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-17 00:16:18 -0800

    Prints a quote to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,6 @@
   */
 int main(void)
 {
-	write(0,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      57);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57);
 	return (1);
 }
