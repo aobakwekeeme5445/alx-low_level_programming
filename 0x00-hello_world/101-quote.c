a9ff697c6a7eec19430eee2d8ee0a69052f12b76
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-16 23:18:39 -0800

    Print a quote to a standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <unistd.h>
 
- /**
+/**
   * main - Entry point
   *
   * Description: A program print a text
   *
-  * Return: Always 0 (Success)
+  * Return: Always 1 (Success)
   */
 int main(void)
 {
-	write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 57);
+	write(0
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 57);
 	return (1);
 }
