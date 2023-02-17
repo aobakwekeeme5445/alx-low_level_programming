313fea9d3d65b2e1e2254722d45bcc73d1b6f3cd
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-16 19:43:34 -0800

    Use puts to print a text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+ 	puts("Programming is like building a multilingual puzzle\n");
+     	return (0);
+}
