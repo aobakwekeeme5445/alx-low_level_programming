da4c57982ec4316f11a4442eb7a6c5b0dd488b80
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-20 09:53:49 -0800

    Prints the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,8 +28,10 @@ int main(void)
 	}
 	if (lastDigit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-	if (lastDigit < 6 && lastDigit > 0)
+	if (lastDigit < 6 && lastDigit != 0)
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 			n, lastDigit);
+	}
 	return (0);
 }
