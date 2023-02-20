124001650cd9592a71b776aa1cff94175998ac91
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-20 11:23:27 -0800

    Prints base 10 digits from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digits numbers
+ * of base 10 starting from 0
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int num = 0; num <= 10; num++)
+		printf("%d", num);
+	printf("%c", '\n');
+	return (0);
+}
