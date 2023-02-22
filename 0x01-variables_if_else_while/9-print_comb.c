abf7a26182eb2d7e2235729330cadd74f2a08937
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-21 20:42:51 -0800

    Prints numbers separated by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints nummbers separated by a
+ * comma and space
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
