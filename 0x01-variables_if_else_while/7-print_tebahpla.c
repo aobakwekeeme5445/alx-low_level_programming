f4bbe4b018f4f301d33dfaa797b6a8557caec143
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-21 20:12:34 -0800

    Prints alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in reverse
+ *
+ * Return: 1 on succession
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+}
