faed601a7b2945880d913eb2e8ea37623e5470c0
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-21 21:33:47 -0800

    Prints combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,12 @@ int main(void)
 	int num2;
 
 	for (num1 = 0; num1 < 9; num1++)
-	{	
+	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
-			
+
 			if (num1 == 8 && num2 == 9)
 				continue;
 
