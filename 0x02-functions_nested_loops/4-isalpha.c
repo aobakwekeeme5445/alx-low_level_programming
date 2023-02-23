7e04727c486e5f468cfa7bb8f362d78182d5f98f
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 03:54:55 -0800

    Checks if character c is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if character c is
+ * an alphabet
+ *
+ * @c: Character checked
+ *
+ * Return: 1 if c is an alphabet, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
