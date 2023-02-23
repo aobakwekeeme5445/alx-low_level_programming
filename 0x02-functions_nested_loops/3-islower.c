deeca3a33e18888e44e5c42a85d0d97cf47898fe
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 03:43:09 -0800

    Checks if c is lowercase or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#incllude "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ *
+ * Return: 0 if c is lowercase, 1 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
