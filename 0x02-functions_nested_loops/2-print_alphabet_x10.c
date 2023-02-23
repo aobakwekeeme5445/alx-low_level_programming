b21f142a9da9391128c0a71e38bfdbb828e474e9
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 03:33:52 -0800

    Prints alphabets 10 times in _abc_

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabets in lowercase
+ * 10 times
+ *
+ * Return: 1 on succession
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int x;
+	
+	for (x = 0; x < 10; x++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+	_putchar('\n');
+	}
+}
