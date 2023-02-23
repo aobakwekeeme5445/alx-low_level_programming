b3e352b15a5ee177029bb42bded3ea6999d44a65
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 04:06:33 -0800

    Check if character n is >0,0, or <0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Trace if character n is neutral,
+ * negative or positive
+ *
+ * @n: Character used to trace
+ *
+ * Return: 1 if greater than 0, -1 if less than 0,
+ * 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+
+	if (n < 0)
+		_putchar('-');
+		return (-1);
+
+	else
+		_putchar('0');
+		return (0);
+}
