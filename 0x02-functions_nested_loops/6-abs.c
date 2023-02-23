991dc45dda94dd84b69400e5b42e90f45eaa7e00
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 04:22:08 -0800

    Computes absolute value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value of an integer
+ *
+ * @int: integer used to return its
+ * absolute value
+ *
+ * Return: O on success
+ */
+int _abs(int)
+{
+	if (int < 0)
+		_putchar(-(int));
+	else
+		_putchar(int);
+
+	return (0);
+}
