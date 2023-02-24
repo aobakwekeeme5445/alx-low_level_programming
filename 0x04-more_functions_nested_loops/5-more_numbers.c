983507e79b3ed94e53c2e1c5a47589ee2df5f697
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 19:21:38 -0800

    Prints 0-14 10times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,18 @@
 void more_numbers(void)
 {
 	int n;
-	int t;
+	int r;
 
-	for (t = 0; t < 9; t++)
+	for (r = 0; r < 10; r++)
 	{
 		for (n = 0; n < 15; n++)
-			_putchar(n + '0');
-
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
