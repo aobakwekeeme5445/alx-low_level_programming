32052e96c9ec1c55ba04063d87cc6c81498fba1b
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 18:45:11 -0800

    Print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		_putchar(n);
+
+	_putchar('\n');
