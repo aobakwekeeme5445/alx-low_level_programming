d3336f95a164a3341e0d8efe4b05746e8bc5bf97
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 18:57:07 -0800

    Prints 0-9 except 2, 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers 0-9 excluding 2, 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int n;
+	
+	for (n = 0; n < 10; n++)
+	{
+		if (n != 2 || n != 4)
+			_putchar(n + '0')
+	}
+	
+	_putchar('\n');
+}
