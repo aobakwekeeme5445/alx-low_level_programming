29c4bd53319243bd952aa04c2af8736576b7182b
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 18:59:54 -0800

    Prints 0-9 except 2, 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@
 void print_most_numbers(void)
 {
 	int n;
-	
+
 	for (n = 0; n < 10; n++)
 	{
-		if (n != 2 || n != 4)
-			_putchar(n + '0')
+		if ((n != 2) || (n != 4))
+			_putchar(n + '0');
 	}
-	
+
 	_putchar('\n');
 }
