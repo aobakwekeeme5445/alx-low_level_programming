a1ebc9e55451b1287ab259392f59d3830138ad9c
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 19:31:32 -0800

    Prints underscore n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Prints line n(_) long
+ *
+ * @n: line distance(_)
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
