1a3897c8567d1598ecffb8dda1198884f8cdc665
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-23 19:45:32 -0800

    Draw \ n times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draw diagonal n times
+ *
+ * @n: times diagonal is drawn
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int col, space;
+
+	if (n > 0)
+	{
+		for (col = 1; col <= n; col++)
+		{
+			for (space = 1; space < col; space++)
+				_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+	else
+		_putchar('\n');
+}
