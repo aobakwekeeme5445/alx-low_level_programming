6f3213aa65214ae677992a92f004a8ff4b53c857
Aobakwe Keeme <aobakwekm@gmail.com>
2023-02-25 02:17:53 -0800

    Test positive_or_negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Test a positive_or_negative() function
+ *
+ * @i: parameter passed to the function
+ *
+ * Return: Always 0 on success
+ */
+int main(void);
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	retun (0);
+}
