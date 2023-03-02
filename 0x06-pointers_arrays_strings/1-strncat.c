eadfffcf6a7a6a4bfb1ab2e0ae787ad01cd9eb59
Aobakwe Keeme <aobakwekm@gmail.com>
2023-03-01 21:53:15 -0800

    Concatenate two strings with some conditions

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two string, it use at
+ * most `n` bytes from source string
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes extracted from source
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0; i;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; i <= n && src[i] != '\0'; i++0)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+
+	return (dest);
+}
