caadd645b827a3c8ca5f7974124e0d1ff53f38b9
Aobakwe Keeme <aobakwekm@gmail.com>
2023-03-02 19:54:53 -0800

    Capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * cap_string - Capitalizes all words of a string.
+  *
+  * @s: separators.
+  *
+  * Return: char value.
+  */
+char *cap_string(char *s)
+{
+	int j = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[j])
+	{
+		i = 0;
+
+		while (i < cspc)
+		{
+			if ((j == 0 || s[j - 1] == spc[i]) && (s[j] >= 97 && s[j] <= 122))
+				s[a] -= 32;
+
+			i++;
+		}
+
+		j++;
+	}
+
+	return (s);
+}
