059ba4b6ccd1625b173dc5f1d5f97d3df7037fcc
Aobakwe Keeme <aobakwekm@gmail.com>
2023-03-02 18:48:35 -0800

    Copies string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,26 +2,27 @@
 
 /**
  * _strncpy: Copies n bytes from the source
- * to destination memory location
+ * to destination memory location.
  *
- * @dest: Destination of the string
- * @src: source of the string
- * @n: number of bytes copied
+ * @dest: Destination of the string.
+ * @src: source of the string.
+ * @n: number of bytes copied.
  *
- * Return: dest
+ * Return: dest.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int slen = 0, i = 0;
 
 	while (src[i++])
-		slen++;
+	slen++;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	for (i = 0; src[i] && i < n; i++)
+	dest[i] = src[i];
 
-	for (i = slen; i < n; index++)
+	for (i = slen; i < n; i++)
 	dest[i] = '\0';
 
 	return (dest);
+
 }
